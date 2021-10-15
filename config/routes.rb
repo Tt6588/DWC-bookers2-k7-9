f5b6f6c1cda676a4fc1389adac338df047884374
Tt6588 <tatsusato1011@gmail.com>
2021-10-15 03:47:21 +0000

    [update]4-10

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy]
   
+  resources :users, only: [:index, :show, :edit, :update,]
+  
 end
