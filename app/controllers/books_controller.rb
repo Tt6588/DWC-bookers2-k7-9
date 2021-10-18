48668128d009376ce6676cbe2897edee902a682c
Tt6588 <tatsusato1011@gmail.com>
2021-10-18 06:23:12 +0000

    [update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,36 @@
 class BooksController < ApplicationController
-  
-  def create
-  end
-  
+
   def index
     @user = current_user
     @book = Book.new
   end
-  
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+
+  end
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
   end
-  
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
