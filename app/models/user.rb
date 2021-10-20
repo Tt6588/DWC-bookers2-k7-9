70fcdf1bd0196bfe573343ad2cd570a7f278cc34
Tt6588 <tatsusato1011@gmail.com>
2021-10-20 04:38:32 +0000

    [update]4-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   attachment :profile_image
   
   
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 } 
+  
+  
 end
