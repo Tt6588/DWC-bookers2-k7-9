2e37cd7cee6b0425ee85d15063321571a4b839c7
Tt6588 <tatsusato1011@gmail.com>
2021-10-29 12:03:23 +0000

    [update]k-4

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,19 @@ Rails.application.routes.draw do
   get '/home/about', to:'homes#index'
   
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
+    # ↓いいね機能
     resource :favorites, only: [:create, :destroy]
+    
+    # ↓コメント機能
     resources :book_comments, only:[:create, :destroy]
   end
   
-  resources :users, only: [:index, :show, :edit, :update,]
+  resources :users, only: [:index, :show, :edit, :update,] do
+    # ↓フォロー・フォロワー機能
+    resource :relation_ships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings'
+    get 'followers' => 'relation_ships#followers'
+  end
   
   
 end
