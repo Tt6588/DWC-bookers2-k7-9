eee6c74c46eee9e31effcb87b0397001a51c6df3
Tt6588 <tatsusato1011@gmail.com>
2021-11-06 07:15:07 +0000

    [add]k-7b 前日比

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
     @book = Book.new
     @user = User.find(params[:id])
     @books = @user.books
+    @today_book = @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @toweek_book = @books.created_toweek
+    @lastweek_book = @books.created_lastweek
   end
 
   private
--- app/models/book.rb
+++ app/models/book.rb
@@ -11,5 +11,10 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 } 
   
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_toweek, -> { where(created_at: Date.today.all_week) }
+  scope :created_lastweek, -> { where(created_at: 1.week.ago.all_day) }
+  
   
 end
